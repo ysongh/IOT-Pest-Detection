bde353643a14314f765ef5d6eca75d88b7521bf5
song <yousonghou@yahoo.com>
2019-05-11 18:58:37 -0400

    use window.setInterval() to change color of the map for every 5 seconds

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
-    height = +svg.attr("height");
-
+    height = +svg.attr("height"),
+    changeColor = false;
 
 var link123 = "http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson";
 d3.json(link123, function(error, nyc) {
   if (error) throw error;
+
+  nycData = nyc;
 
   var path = d3.geoPath()
       .projection(d3.geoConicConformal()
@@ -20,7 +22,7 @@ d3.json(link123, function(error, nyc) {
       .style("fill", function(d) {
         switch(d.properties.borough){
           case 'Queens':
-            return "blue";
+            return 'pink';
           case 'Bronx':
             return "green";
           case 'Brooklyn':
@@ -59,63 +61,21 @@ d3.json(link123, function(error, nyc) {
     console.log(nyc);
 });
 
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
-
-
-var link123 = "http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson";
-d3.json(link123, function(error, nyc) {
-  if (error) throw error;
-
-  var path = d3.geoPath()
-      .projection(d3.geoConicConformal()
-      .parallels([33, 45])
-      .rotate([96, -39])
-      .fitSize([width, height], nyc));
-
+window.setInterval(function(){
+  changeColor = !changeColor;
   svg.selectAll("path")
-      .data(nyc.features)
-      .enter().append("path")
-      .attr("d", path)
       .style("fill", function(d) {
         switch(d.properties.borough){
           case 'Queens':
-            return "blue";
+            return changeColor ? 'gold' : 'orange';
           case 'Bronx':
-            return "green";
+            return changeColor ? 'grey' : "blue";
           case 'Brooklyn':
-            return "red";
+            return changeColor ? 'red' : "yellow";
           case 'Manhattan':
-            return 'orange';
+            return changeColor ? 'blue' : 'green';
           default:
-            return 'black';
+            return changeColor ? 'black' : 'red';
         }
-        return d.properties.borough === 'Queens' ? "blue": "black"
       })
-      .on("mouseenter", function(d) {
-        console.log(d);
-      d3.select(this)
-      .style("stroke-width", 1.5)
-      .style("stroke-dasharray", 0)
-
-      d3.select("#neighborhoodPopover")
-      .transition()
-      .style("opacity", 1)
-      .style("color", (d.properties.borough === 'Queens' ? "blue": "black"))
-      .style("left", (d3.event.pageX) + "px")
-      .style("top", (d3.event.pageY) + "px")
-      .text(d.properties.neighborhood + " "+ d.properties.boroughCode)
-    })
-    .on("mouseleave", function(d) { 
-      d3.select(this)
-      .style("stroke-width", .25)
-      .style("stroke-dasharray", 1)
-
-      d3.select("#cneighborhoodPopoverountyText")
-      .transition()
-      .style("opacity", 0);
-    });
-
-    console.log(nyc);
-});
+}, 2000);
